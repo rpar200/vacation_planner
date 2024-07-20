b88133553416f83bbaab82fd9125edace644c71e
Rufus Park <rpar200@wgu.edu>
2024-07-20 13:45:54 -0400

    Added controllers and UI elements for a login and registration page using FireBase authentication. Validation was included with accounts to ensure email or password fields are populated and password is eight or more characters.

---
+++ app/src/main/java/com/erde/erde_vacation_planner/UI/Login.java
@@ -0,0 +1,94 @@
+package com.erde.erde_vacation_planner.UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.erde.erde_vacation_planner.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+    FirebaseAuth auth;
+    String email;
+    String password;
+
+    EditText mEmail;
+    EditText mPassword;
+    Button mLoginBtn;
+    TextView newAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+        auth = FirebaseAuth.getInstance();
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        mLoginBtn = findViewById(R.id.login);
+        newAccount = findViewById(R.id.newaccount);
+
+        email = getIntent().getStringExtra("email");
+        password = getIntent().getStringExtra("password");
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is required.");
+                    return;
+                }
+
+                if(password.length() < 8){
+                    mPassword.setError("Password must be at least 8 characters.");
+                }
+
+              else {
+                    auth.signInWithEmailAndPassword(email, password)
+                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    Toast.makeText(Login.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(Login.this, MainActivity.class));
+                                    finish();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(Login.this, "Login unsuccessful.", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                }
+            }
+        });
+
+        newAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Registration.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/erde/erde_vacation_planner/UI/Registration.java
@@ -0,0 +1,77 @@
+package com.erde.erde_vacation_planner.UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.erde.erde_vacation_planner.R;
+
+public class Registration extends AppCompatActivity {
+    String email;
+    String password;
+
+    Button register;
+    FirebaseAuth auth;
+
+    EditText mEmail;
+    EditText mPassword;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registration);
+        super.onCreate(savedInstanceState);
+
+        auth = FirebaseAuth.getInstance();
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user  = mEmail.getText().toString().trim();
+                String pass = mPassword.getText().toString().trim();
+
+                if(user.isEmpty()){
+                    mEmail.setError("Email is required.");
+                    return;
+                }
+
+                if(pass.isEmpty()){
+                    mPassword.setError("Password is required.");
+                    return;
+                }
+
+                if(pass.length() < 8){
+                    mPassword.setError("Password must be at least 8 characters.");
+                } else {
+                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isComplete()) {
+                                Toast.makeText(Registration.this, "Account created!", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(Registration.this, Login.class));
+                            } else {
+                                Toast.makeText(Registration.this, "Account creation unsuccessful. ", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        }); 
+    }
+}
